1e9d35e9cb77f3a52efab1db21b7ce255c571a89
SanthoshSanthoju <70399715+SanthoshSanthoju@users.noreply.github.com>
2025-04-13 06:04:43 +0000

    container config

---
+++ Basics/variables.js
@@ -0,0 +1,7 @@
+const accountId =55
+let accountName ="Santhosh"
+var city = "Hyderabad"
+//accountId =2
+let accountBalance
+console.log(accountId)
+console.table([accountId,accountName,city,accountBalance])
